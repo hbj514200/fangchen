4e8b66e340bfd89536d46194db424ac6eb6561f3
hbj514200 <1336892373@qq.com>
2016-10-01 23:22:33 +0800

    create

---
+++ app/src/main/java/com/qq/qzone/a1336892373/fangchen/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.qq.qzone.a1336892373.fangchen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try { Thread.sleep(1500); } catch (Exception e) { }
+                //startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        }).start();
+    }
+
+}
---
+++ app/src/test/java/com/qq/qzone/a1336892373/fangchen/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.qq.qzone.a1336892373.fangchen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
